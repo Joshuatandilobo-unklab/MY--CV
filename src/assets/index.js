5f39625a18038c074a30f61e75119c0406535099
Joshuatandilobo-unklab <joshuawowiling456@gmail.com>
2024-11-21 18:25:09 +0700

    adding joshua foto

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,7 @@
+import ProfilePicture from "./joshua.jfif";
 import phoneIcon from "./phone.png";
 import instagramIcon from "./instagram.png";
 import githubIcon from "./github.png";
 import facebookIcon from "./facebook.png";
 
-export { phoneIcon, instagramIcon, githubIcon, facebookIcon };
+export { ProfilePicture, phoneIcon, instagramIcon, githubIcon, facebookIcon };
